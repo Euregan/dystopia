9ee7509b1b246eedccf52356f98808853bbb0e6a
Valentin Manceaux-Panot <va.mcxp@gmail.com>
2021-05-01 17:45:07 +0200

    Changed the docs style

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,12 @@
 import '../firacode.css'
 import '../global.css'
+import './preview.css'
+import { themes } from '@storybook/theming'
 
 export const parameters = {
   layout: 'centered',
+  docs: {
+    theme: themes.dark,
+    page: null,
+  },
 }
