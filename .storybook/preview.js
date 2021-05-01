5240686dba97f1f3b4db6f3ce572189a3d5784d6
Valentin Manceaux-Panot <va.mcxp@gmail.com>
2021-05-01 11:54:13 +0200

    tmp

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,16 @@
 import '../firacode.css'
 import '../global.css'
+import { themes } from '@storybook/theming'
 
 export const parameters = {
   layout: 'centered',
+  backgrounds: {
+    default: 'dystopia',
+    values: [
+      { name: 'dystopia', value: 'linear-gradient(to left, #0b0b2b, #210c1e)' },
+    ],
+  },
+  docs: {
+    theme: themes.dark,
+  },
 }
